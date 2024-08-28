c39b200f19e58e0cd2528b039254a9a49cb5f073
gabrielhenriqueortela <108945042+gabrielhenriqueortela@users.noreply.github.com>
2024-08-28 09:02:10 -0300

    Add files via upload

---
+++ Trabalho--main/Trabalho--main/script/script.js
@@ -0,0 +1,72 @@
+let entrevista_1 = {
+  entrevistador: "Gabriel Ortela"
+  entrevistado: "João Victor"
+    questoes: {
+      p1: "O que você mais tirou proveito do colégio?"
+      r1: "Fazer amizades e aprender a conviver na sociedade"
+
+      p2: "O que você pretende fazer depois do colégio?"
+      r2: "Trabalhar e fazer uma faculdade de agronomia"
+
+      p3: "Qual era sua profissão do sonho quando era criança?"
+      r3: "Astronauta"
+
+      p4: "Hoje em dia qual e o seu sonho de profissão?"
+      r4: "Ser agricultor"
+
+      p5: "Se esse sonho não se realizar, qual a sua segunda opção?"
+      r5: "Ser caminhoneiro"
+
+      p6: "Como você se interesou por sua profissão de sonho?"
+      r6: "Pela minha familia"
+
+      p7: "Você pretende casar?"
+      r7: "Sim"
+
+      p8: "Você pretende ter filhos,se sim quantos?"
+      r8: "Sim,pretendo ter uns 2 filhos"
+
+      p9: "O que você gosta de fazer nas horas vagas?"
+      r9: "Jogar video game"
+      
+      p10: "Você acha que vai sentir falta da sua época de criança,se sim porque?"
+      r10:""
+
+    }
+}
+let entrevista_2 = {
+  entrevistador: "João Victor"
+  entrevistado: "Gabriel Ortela"
+    questoes: {
+      p1: "O que você mais tirou proveito do colégio?"
+      r1: ""
+
+      p2: "O que você pretende fazer depois do colégio?"
+      r2: ""
+
+      p3: "Qual era sua profissão do sonho quando era criança?"
+      r3: ""
+
+      p4: "Hoje em dia qual e o seu sonho de profissão?"
+      r4: ""
+
+      p5: "Se esse sonho não se realizar, qual a sua segunda opção?"
+      r5: ""
+
+      p6: "Como você se interesou por sua profissão de sonho?"
+      r6: ""
+
+      p7: "Você pretende casar?"
+      r7: ""
+
+      p8: "Você pretende ter filhos,se sim quantos?"
+      r8: ""
+
+      p9: "O que você gosta de fazer nas horas vagas?"
+      r9: ""
+
+      p10: "Você acha que vai sentir falta da sua época de criança,se sim porque?"
+      r10:""
+
+    }
+}
